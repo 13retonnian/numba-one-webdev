d7cc35ca67d408f45b2bfa5b8acd93595a17656f
Filora T <filorat@outlook.com>
2021-11-30 17:12:04 -0700

    tweaks

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -5,29 +5,29 @@
     <ul>
       <li v-for="blog in blogs" :key="blog.slug">
         <NuxtLink :to="`blog/${blog.slug}`"> {{ blog.title }}</NuxtLink>
-    </li>
+      </li>
     </ul>
-    
   </v-container>
 </template>
 
 <script>
 export default {
-   async asyncData({ $content, params }) {
-    const blogs = await $content('blog').fetch()
+  async asyncData({ $content, params }) {
+    const blogs = await $content("blog").fetch();
     return {
       blogs,
-    }
+    };
   },
   head() {
     return {
       script: [
-        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+      ],
     };
   },
 
   generate: {
-    fallback: true
+    fallback: true,
   },
-}
+};
 </script>
