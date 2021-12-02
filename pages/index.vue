94a90ecb6807fcb7d3bad7d2f6efa455492daa3c
Filora T <filorat@outlook.com>
2021-12-01 23:21:59 -0700

    edits

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,19 @@
 <template>
-  <v-container>
-  </v-container>
+  <v-container> </v-container>
 </template>
 <style>
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 1rem;
-    line-height: 1.6;
-    background-image: url("static/assets/images/design-3.svg");
-    background-repeat: no-repeat;		
-    background-position: top;	
-    background-size: cover;
-    height: 100vh; 
-    background-attachment: fixed;
-  }
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1rem;
+  line-height: 1.6;
+  background-image: url("static/assets/images/design-3.svg");
+  background-repeat: no-repeat;
+  background-position: top;
+  background-size: cover;
+  height: 100vh;
+  background-attachment: fixed;
+}
+.navlink {
+  padding-left: 2rem;
+}
 </style>
