2450b836c180f8732e3f9d7adfd72931a9f3c5d4
Filora T <filorat@outlook.com>
2021-12-01 21:39:20 -0700

    added component, fixed index.vue

---
+++ components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <figure>
+      <div v-html="`<h3>${info.title}</h3>`"></div>
+      <nuxt-img :src="info.image" sizes="sm:300px" />
+      <figcaption v-text="info.description" />
+      <a href=""></a>
+    </figure>
+  </div>
+</template>
+<script>
+export default {
+  name: "Card",
+  props: ["pageInfo", "info"],
+};
+</script>
+<!-- CSS -->
+<style>
+.card {
+  background: rgb(243, 243, 243);
+  padding: 20px;
+  width: 340px;
+  justify-content: center;
+}
+figcaption {
+  display: inline-block;
+  padding-top: 1rem;
+}
+</style>
--- pages/team/index.vue
+++ pages/team/index.vue
@@ -3,8 +3,13 @@
     <Header :pageInfo="siteInfo" />
     <header>
       <h1>{{ siteInfo.pageName }}</h1>
-      <main class="grid-box">
-        <Cards v-for="topic in topics" :key="topic.id" :info="topic" />
+      <main class="flex justify-evenly gap-x-16 gap-y-16 flex-wrap p-16">
+        <Card
+          :pageInfo="siteInfo"
+          v-for="card in topics"
+          :key="card.id"
+          :info="card"
+        />
       </main>
     </header>
   </div>
@@ -20,22 +25,26 @@ export default {
       },
       topics: [
         {
+          image: "assets/images/icah.png",
           title: "I'm Icha",
-          img: "images/icah.png",
+          description:
+            "Aspiring pro front end developer. When I'm not in front of my computer, I enjoy hiking, walking my dog, having tea or coffee and eating chocolate. Trying to live life to the fullest!",
         },
         {
+          image: "assets/images/chris.png",
           title: "I'm Chris",
-          img: "images/chris.png",
+          description: "",
         },
         {
+          image: "assets/images/filora.png",
           title: "I'm Filora",
-
-          img: "images/filora.png",
+          description:
+            "I strive for beautiful and functional designs. My current life motto is to learn and explore the possibilities of programming languages. ",
         },
         {
+          image: "assets/images/james.png",
           title: "I'm James",
-
-          img: "images/james.png",
+          description: "",
         },
       ],
     };
@@ -45,12 +54,16 @@ export default {
 
 <style>
 body {
-  background-color: white;
+  /* background-color: white; */
 }
 .grid-box {
-  display: grid;
+  /* display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 10px;
-  text-align: center;
+  text-align: center; */
+}
+.figcaption {
+  margin: 0;
+  display: inline-block;
 }
 </style>
