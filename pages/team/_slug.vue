e4183b750dc47c78e5991b5d998d4c964521853b
Filora T <filorat@outlook.com>
2021-12-01 23:04:51 -0700

    slug vue added for team

---
+++ pages/team/_slug.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-main>
+    <v-container>
+      <h1>{{ siteInfo.title }}</h1>
+      <section>
+        <ul>
+          <li>Image: {{ siteInfo.image }}</li>
+          <li>Title: {{ siteInfo.title }}</li>
+          <li>Description {{ siteInfo.description }}</li>
+        </ul>
+      </section>
+      <section>
+        <nuxt-content :document="siteInfo" />
+      </section>
+    </v-container>
+  </v-main>
+</template>
+<script>
+export default {
+  props: ["siteInfo"],
+};
+</script>
